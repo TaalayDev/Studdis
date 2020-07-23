df44efcbfba61218ac01f2a8bdfa5b08645fb639
TaalayDev <a.u.taalay@gmail.com>
2020-07-23 19:33:52 +0600

    .

---
+++ app/src/main/java/com/zoro/studdis/ui/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.zoro.studdis.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zoro.studdis.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
